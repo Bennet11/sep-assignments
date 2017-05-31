f93d855f21b212449bcdcd5d40fd91e69a62c12d
Bennet11 <bennetveloya@gmail.com>
2017-05-31 00:24:35 -0700

    still needs improvements linked_list

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -6,25 +6,64 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if @head == nil
+      @head = node
+      @tail = @head
+    else
+      @tail.next = node
+      @tail = @tail.next
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if @tail == @head
+      @head = nil
+      @tail = nil
+    else
+      @tail = node
+      @tail.next = nil
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    node = @head
+    until node == nil do
+      puts node.data
+      node = node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      remove_front
+    elsif @tail == node
+      remove_tail
+    else
+      
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    if @head == nil?
+      @head = node
+    else
+      node.next = @head
+      @head = node
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    if @head === @tail
+      @head = nil
+      @tail = nil
+    elsif @head != nil
+      node = @head.next
+      @head = node
+    end
   end
 end
