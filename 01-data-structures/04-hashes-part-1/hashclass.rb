3590ac0e840fcb2a24fc16e3f8490e6b3e66d78c
Bennet11 <bennetveloya@gmail.com>
2017-06-05 23:31:26 -0700

    hashes part 1 in progress

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,6 +5,14 @@ class HashClass
   end
 
   def []=(key, value)
+    item = HashItem.new(key, value)
+    i = index(key, value)
+    if @items[i] == nil
+      @items[i] = HashItem.new(key, value)
+    elsif @items[i].key == item.key && @items[i].value == item.value
+      self.resize
+    end
+
   end
 
 
@@ -12,16 +20,23 @@ class HashClass
   end
 
   def resize
+    old_items = @items.compact
+    @items = Array.new(size * 2)
+    old_items.each do |item|
+      self[item.key] = item.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
 end
