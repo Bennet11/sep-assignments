b8bb839f8747ec6159ec197b7c24dacbee9252e2
Ben Neely <ben.neely@kount.com>
2017-07-22 16:36:27 -0600

    Modify specs, node, and film

--- 01-data-structures/07-graphs/kevin_bacon_finder_spec.rb
+++ 01-data-structures/07-graphs/kevin_bacon_finder_spec.rb
@@ -3,66 +3,58 @@ require_relative 'film'
 require_relative 'kevin_bacon_finder'
 
 RSpec.describe "kevin_bacon_finder" do
-  let(:kevin_bacon) { Node.new("Kevin Bacon") }
+  let(:bacon) { Node.new("Kevin Bacon") }
 
-  let(:liam_neeson) { Node.new("Liam Neeson") }
-  let(:brad_pitt) { Node.new("Brad Pitt") }
-  let(:ryan_reynolds) { Node.new("Ryan Reynolds") }
-  let(:will_smith) { Node.new("Will Smith") }
-  let(:al_pacino) { Node.new("Al Pacino") }
   let(:robert_de_niro) { Node.new("Robert De Niro") }
   let(:ray_liotta) { Node.new("Ray Liotta") }
   let(:joe_pesci) { Node.new("Joe Pesci") }
-  let(:kim_dickens) { Node.new("Kim Dickens") }
-  let(:elisabeth_shue) { Node.new("Elisabeth Shue") }
-  let(:samuel_jackson) { Node.new("Samuel Jackson") }
-  let(:jack_nickolson) { Node.new("Jack Nickolson") }
-  let(:john_travolta) { Node.new("John Travolta") }
-  let(:leonardo_di_caprio) { Node.new("Leonardo Di Caprio") }
+  let(:al_pacino) { Node.new("Al Pesci") }
+  let(:liam_neeson) { Node.new("Liam Neeson") }
+  let(:ray_reynolds) { Node.new("Ray Reynolds") }
+  let(:will_smith) { Node.new("Will Smith") }
   let(:mel_gibson) { Node.new("Mel Gibson") }
-  let(:tom_cruise) { Node.new("Tom Cruise") }
-  let(:clint_eastwood) { Node.new("Clint Eastwood") }
-  let(:angelina_jolie) { Node.new("Angelina Jolie") }
-  let(:natalie_portman) { Node.new("Natalie Portman") }
+	let(:leonardo_di_caprio) { Node.new("Leonardo Di Caprio") }
+	let(:jack_nickolson) { Node.new("Jack Nickolson") }
+	let(:clint_eastwood) { Node.new("Clint Eastwood") }
+	let(:john_travolta) { Node.new("John Travolta") }
+	let(:samuel_jackson) { Node.new("Samuel Jackson") }
+	let(:tom_cruise) { Node.new("Tom Cruise") }
+	let(:elisabeth_shue) { Node.new("Elisabeth Shue") }
+	let(:kim_dickens) { Node.new("Kim Dickens") }
+	let(:natalie_portman) { Node.new("Natalie Portman") }
+	let(:ryan_reynolds) { Node.new("Ryan Reynolds") }
 
-  let(:goodfellas) { Film.new("Goodfellas", [robert_de_niro, ray_liotta, joe_pesci]) }
-  let(:god_father) { Film.new("God Father", [al_pacino]) }
-  let(:schindler_list) { Film.new("Schindler List", [liam_neeson]) }
-  let(:life) { Film.new("Life", [ryan_reynolds]) }
-  let(:pursuit_of_happiness) { Film.new("Pursuit of Happiness", [will_smith]) }
-  let(:braveheart) { Film.new("Braveheart", [mel_gibson]) }
-  let(:blood_diamond) { Film.new("Blood Diamond", [leonardo_di_caprio]) }
-  let(:the_shining) { Film.new("The Shining", [jack_nickolson]) }
-  let(:gran_torino) { Film.new("Gran Torino", [clint_eastwood]) }
-  let(:pulp_fiction) { Film.new("Pulp Fiction", [john_travolta, samuel_jackson]) }
-  let(:top_gun) { Film.new("Top Gun", [tom_cruise]) }
-  let(:hollow_man) { Film.new("Hollow Man", [kevin_bacon, elisabeth_shue, kim_dickens]) }
-  let(:v_for_vendetta) { Film.new("V for Vendetta", [natalie_portman]) }
+  let(:ortiz) {Node.new("Xavier Ortiz")}
+
 
   describe "#find_kevin_bacon" do
-    it "returns an empty array if starting node has Kevin Bacon" do
-      expect(find_kevin_bacon(kevin_bacon)).eq []
+		before(:each) do
+			@goodfellas = Film.new("Goodfellas", [robert_de_niro, ray_liotta, joe_pesci])
+			@god_father = Film.new("God Father", [al_pacino])
+			@schindler_list = Film.new("Schindler List", [liam_neeson])
+			@life = Film.new("Life", [ryan_reynolds])
+			@pursuit_of_happiness = Film.new("Pursuit of Happiness", [will_smith])
+			@braveheart = Film.new("Braveheart", [mel_gibson])
+			@blood_diamond = Film.new("Blood Diamond", [leonardo_di_caprio])
+			@the_shining = Film.new("The Shining", [jack_nickolson])
+			@gran_torino = Film.new("Gran Torino", [clint_eastwood])
+			@pulp_fiction = Film.new("Pulp Fiction", [john_travolta, samuel_jackson])
+			@top_gun = Film.new("Top Gun", [tom_cruise])
+			@hollow_man = Film.new("Hollow Man", [bacon, elisabeth_shue, kim_dickens])
+			@v_for_vendetta = Film.new("V for Vendetta", [natalie_portman])
+			@x_men_first_class = Film.new("X-Men: First Class", [bacon, natalie_portman])
+		end
+
+    it "returns an empty array if the node is Kevin Bacon" do
+      expect(find_kevin_bacon(bacon)).to eq []
     end
 
-    it "finds Kevin bacon with a maximum of distance of 6" do
-      # start with an actor node who is not kevin, and return a list of 6 movies, the last of which he's in
-      expect(find_kevin_bacon(john_travolta)).to eq(pulp_fiction)
-      expect(find_kevin_bacon(al_pacino)).to eq(god_father)
-      expect(find_kevin_bacon(will_smith)).to eq(pursuit_of_happiness)
-      expect(find_kevin_bacon(robert_de_niro)).to eq(goodfellas)
-      expect(find_kevin_bacon(mel_gibson)).to eq(braveheart)
-      expect(find_kevin_bacon(elisabeth_shue)).to eq(hollow_man)
-
+    it "finds kevin bacon within two jumps " do
+      expect(find_kevin_bacon(natalie_portman)).to eq ["V for Vendetta", "X-Men: First Class"]
     end
 
-    it "returns nil if distance to Kevin Bacon is greater than 6" do
-      # start with an actor node who is not kevin, who isn't within 6 movies of kevin, return nil
-      expect(find_kevin_bacon(tom_cruise)).to eq nil
-    end
-
-    it "returns nil if an actor node is not in any movies" do
-      # start with an actor who isn't in any movies, return nil
-      expect(find_kevin_bacon(michael_fassbender)).to eq nil
+    it "returns [] if an actor node is not in any movies" do
+      expect(find_kevin_bacon(ortiz)).to eq []
     end
   end
 end
